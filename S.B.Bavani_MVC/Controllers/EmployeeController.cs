2b9ff669b802f9f1d6bbd36debdfee4fdaa7711c
Bavani43 <bavanisb481@gmail.com>
2025-11-03 08:40:56 +0530

    Added EmployeeForm & EmployeeList with MODEL

---
+++ S.B.Bavani_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using S.B.Bavani_MVC.Models;
+
+
+
+namespace S.B.Bavani_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult EmployeeForm()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("employee/get")]
+        public JsonResult Form( EmployeeInfo inputData, string firstName, string lastName, string userName, string email, string password)
+        {
+            if (inputData != null &&
+                (inputData.FirstName == firstName && inputData.LastName == lastName && inputData.UserName == userName && inputData.Password == password && inputData.Email == email))
+            {
+                return Json(new
+                {
+                    success = true,
+                    message = "Employee added successfully"
+                });
+            }
+
+            return Json(new
+            {
+                success = false,
+                message = "Failed."
+            });
+        }
+
+        public IActionResult EmployeeList()
+        {
+            var employees = new List<EmployeeInfo>
+            {
+                new EmployeeInfo { FirstName="Bavani", LastName="S", UserName="bavani", Password="1234", Email="bavani@gmail.com" }
+            };
+
+            return View(employees);
+        }
+    }
+}
---
+++ S.B.Bavani_MVC/wwwroot/js/Employee.js
@@ -0,0 +1,78 @@
+﻿function isValidEmployee() {
+    var firstName = $("#empFirstName").val();
+    var lastName = $("#empLastName").val();
+    var userName = $("#empUserName").val();
+    var password = $("#empPassword").val();
+    var email = $("#empEmail").val();
+
+
+    var isValid = true;
+
+    if (firstName == "") {
+        $("#firstNameError").show();
+        isValid = false;
+    } else {
+        $("#firstNameError").hide();
+    }
+
+    if (lastName == "") {
+        $("#lastNameError").show();
+        isValid = false;
+    } else {
+        $("#lastNameError").hide();
+    }
+    if (userName == "") {
+        $("#userNameError").show();
+        isValid = false;
+    } else {
+        $("#userNameError").hide();
+    }
+    if (password == "") {
+        $("#passwordError").show();
+        isValid = false;
+    } else {
+        $("#passwordError").hide();
+    }
+    if (email == "") {
+        $("#emailError").show();
+        isValid = false;
+    } else {
+        $("#emailError").hide();
+    }
+
+    return isValid;
+}
+
+function addEmployee() {
+    if (isValidEmployee()) {
+        var firstName = $("#empFirstName").val();
+        var lastName = $("#empLastName").val();
+        var userName = $("#empUserName").val();
+        var password = $("#empPasword").val();
+        var email = $("#empEmail").val();
+
+
+        var emp = {
+            FirstName: firstName,
+            LastName: lastName,
+            UserName: userName,
+            Password: password,
+            Email: email
+        };
+        $.ajax({
+            url: '/employee/insert',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(emp),
+            success: function (response) {
+                alert(response.message && "Employee added successfully");
+                clearEmployeeForm();
+                loadEmployeeList();
+            },
+            error: function () {
+                alert("Failed");
+            }
+
+        });
+    }
+}
